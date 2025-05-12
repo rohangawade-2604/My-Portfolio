0b78b3681c4421488e778baa24a91559a8402099
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-13 00:50:45 +0530

    just working on my project works

--- RNG-Portfolio/src/Portfolio_Component/Portfolio.jsx
+++ RNG-Portfolio/src/Portfolio_Component/Portfolio.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 const Portfolio = () => {
   return (
     <>
-      <div className='Portfolio_Part'>
-        <h1>Hello Rohan , Welcome to Portfolio page</h1>
+      <div className='Portfolio_Part bg-black text-white h-200 w-full relative'>
+        
+        <h1 className='text-[110px] font-bold text-center absolute left-140 opacity-15'>WORKS</h1>
+        <h1 className='text-center font-bold text-[45px] pt-15'>MY <span className='text-amber-400'>PORTFOLIO</span></h1>
       </div>
     </>
   )
