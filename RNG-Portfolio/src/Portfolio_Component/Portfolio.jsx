5fb060efca3ddcb030082a9247ab832dffff3ece
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-18 00:08:12 +0530

    just make fully part

--- RNG-Portfolio/src/Portfolio_Component/Portfolio.jsx
+++ RNG-Portfolio/src/Portfolio_Component/Portfolio.jsx
@@ -1,3 +1,4 @@
+import  { useState } from 'react'
 import React from 'react'
 import AB from "../assets/AB.png"; // correct
 import CW from "../assets/CW.png"
@@ -6,78 +7,97 @@ import DP from "../assets/DP.png"
 import md from "../assets/md.png"
 import TL from "../assets/TL.png"
 
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 export const Portfolio = () => {
+
+    const [data , setData] = useState("All");
 
   const Projects = [
     {
       id:1,
       img:AB,
-      title:"Aussies Buckets Website"
+      title:"Aussies Buckets Website",
+      category:"react"
     },
 
     {
       id:2,
       img:CW,
-      title: "Coffee Delite Website"
+      title: "Coffee Delite Website",
+      category:"HCJ",
     },
 
     {
       id:3,
       img:CD,
-      title:"Creative Company Website"
+      title:"Creative Company Website",
+      category:"HCJ",
     },
 
     {
       id:4,
       img:DP,
-      title:"Gym Portfolio Website"
+      title:"Gym Portfolio Website",
+      category:"react",
     },
 
     {
       id:5,
       img:md,
-      title:"1MDM website"
+      title:"1MDM website",
+      category:"react",
     },
 
     {
       id:6,
       img:TL,
-      title:"Todo List"
+      title:"Todo List",
+      category:"HCJ",
     },
 
-    {
-      id:7,
-      img:AB,
-      title:"Aussies"
-    }
+
+]
 
 
+  const filterData = data === "All" ? Projects : Projects.filter((el) => {
+    return el.category === data;
+  })
 
-  ]
+    useEffect(() => {
+          AOS.init({
+            duration: 1000, // Animation duration in milliseconds
+            easing: 'ease-out-cubic', // Animation easing
+            once: true, // Whether animation should happen only once
+            disable: false // Disable AOS on mobile devices
+          });
+        }, []);
 
   return (
     <>
-      <div className='Portfolio_Part bg-black text-white h-200 w-full relative'>
+      <div className='Portfolio_Part bg-black text-white h-250 w-full relative'>
         
         <h1 className='text-[110px] font-bold text-center absolute left-140 ml-5 opacity-15'>WORKS</h1>
         <h1 className='text-center font-bold text-[45px] ml-8 pt-15'>MY <span className='text-amber-400'>PORTFOLIO</span></h1>
 
-        <div className="button mt-15 text-center ">
-          <button className='ml-15 font-semibold cursor-pointer'>ALL</button>
-          <button className='ml-15 font-semibold cursor-pointer'>HTML & CSS & JAVASCRIPT</button>
-          <button className='ml-15 font-semibold cursor-pointer'>REACT.JS</button>
-          <button className='ml-15 font-semibold cursor-pointer'>TAILWIND CSS</button>
+        <div className="button mt-15 text-center " data-aos="fade-up">
+          <button onClick={() => setData("All")} className='ml-15 font-semibold cursor-pointer'>ALL</button>
+          <button onClick={() => setData("HCJ")} className='ml-15 font-semibold cursor-pointer'>HTML & CSS & JAVASCRIPT</button>
+          <button onClick={() => setData("react")} className='ml-15 font-semibold cursor-pointer'>REACT.JS & TAILWIND CSS</button>
+          
 
         </div>
 
 
-        <div className="Content-part grid grid-cols-3">
+        <div className="Content-part grid grid-cols-3 my-20 justify-items-center mx-23 gap-10">
           {
-            Projects.map((el , id) => (
-              <div className=''>
-                 <div className="card text-white ">
-                <img src={el.img} alt="" key={id} className='h-20'/>
-                <h1>{el.title}</h1>
+            filterData.map((el , id) => (
+              <div className='' data-aos="fade-up">
+                 <div className="card text-white text-center font-bold text-[17px]" data-aos="fade-up">
+                <img src={el.img} alt={el.category || "project"} key={id} className='h-40 mt-10' data-aos="fade-up"/>
+                <h1 className='mt-5' data-aos="fade-up">{el.title}</h1>
               </div>
               </div>
              
