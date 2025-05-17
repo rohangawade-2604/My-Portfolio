27562c634fcc8fd25a5034679f25a8a7f46daf0c
rohangawade-2604 <rohangawade2604@gmail.com>
2025-05-18 03:04:23 +0530

    done with everything

--- RNG-Portfolio/src/AboutComponent/About.jsx
+++ RNG-Portfolio/src/AboutComponent/About.jsx
@@ -24,7 +24,7 @@ export const About = () => {
 
   return (
     <>
-      <div className='h-450 px-40 text-white bg-black relative dark:bg-white dark:text-black pt-25'  >
+      <div className='h-450 px-40 text-white bg-black relative dark:bg-white dark:text-black pt-25 overflow-hidden'  >
         <h1 className='text-center text-[110px] font-bold opacity-15 absolute left-138' >RESUME</h1>
         <h1 className='text-[60px] font-bold text-center pt-10'  data-aos="fade-up" >ABOUT <span className='text-amber-300'  data-aos="fade-up">ME</span></h1>
 
